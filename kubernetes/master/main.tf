e27043c7dc0d0bb6825831acb51ae6ecc604b3cf
enxebre <alberto.garcial@hotmail.com>
2016-05-23 12:03:51 +0100

    Safe typing parameters.

--- kubernetes/master/main.tf
+++ kubernetes/master/main.tf
@@ -6,7 +6,14 @@ variable "dns_names" { default = "" }
 # for internal cert but use the public_ip to connect via ssh
 variable "deploy_ssh_hosts" {}
 variable "master_count" {}
-variable "kube_service_ip" { default = "10.3.0.1" }
+
+variable "kube_service_ip" { 
+  type = "map"
+  default = {
+    0 = "10.3.0.1"
+  }
+}
+
 variable "validity_period_hours" { default = "8760" }
 variable "early_renewal_hours" { default = "720" }
 variable "ssh_user" { default = "core" }
