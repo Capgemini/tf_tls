4709eb8dbedc0976ffbff8bc1c1ab0ea536d22ea
Marc Tamsky <tamsky@users.noreply.github.com>
2016-11-15 19:06:08 -0800

    update for terraform 0.7.x

--- docker/client/main.tf
+++ docker/client/main.tf
@@ -1,9 +1,9 @@
 variable "ca_cert_pem" {}
 variable "ca_private_key_pem" {}
-variable "ip_addresses_list" {}
+variable "ip_addresses_list" { type = "list" }
 # supports if you have a public/private ip and you want to set the private ip
 # for internal cert but use the public_ip to connect via ssh
-variable "deploy_ssh_hosts" {}
+variable "deploy_ssh_hosts" { type = "list" }
 variable "dns_names_list" { default = "*.*.cluster.internal,*.ec2.internal" }
 variable "docker_client_count" {}
 variable "private_key" {}
