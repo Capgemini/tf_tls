b62b241ca4423d0630f4c3bd59af0f08e080c16e
Marc Tamsky <tamsky@users.noreply.github.com>
2016-11-15 19:04:23 -0800

    update for terraform-0.7.x

--- kubernetes/master/main.tf
+++ kubernetes/master/main.tf
@@ -4,7 +4,7 @@ variable "ip_addresses" { type = "list" }
 variable "dns_names" { default = "" }
 # supports if you have a public/private ip and you want to set the private ip
 # for internal cert but use the public_ip to connect via ssh
-variable "deploy_ssh_hosts" {}
+variable "deploy_ssh_hosts" { type = "list" }
 variable "master_count" {}
 variable "kube_service_ip" { default = "10.3.0.1" }
 variable "validity_period_hours" { default = "8760" }
