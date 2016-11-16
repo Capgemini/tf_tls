2bcb4802c3a50393934f2eddadecd29b334a775d
Marc Tamsky <tamsky@users.noreply.github.com>
2016-11-15 22:44:36 -0800

    update triggers->ip_addresses for terraform 0.7
    testing

--- kubernetes/master/deploy.tf
+++ kubernetes/master/deploy.tf
@@ -8,7 +8,7 @@ resource "null_resource" "configure-master-certs" {
     validity_period_hours = "${var.validity_period_hours}"
     early_renewal_hours   = "${var.early_renewal_hours}"
     dns_names             = "${var.dns_names}"
-    ip_addresses          = "${var.ip_addresses}"
+    ip_addresses          = [ "${var.ip_addresses}" ]
   }
 
   connection {
